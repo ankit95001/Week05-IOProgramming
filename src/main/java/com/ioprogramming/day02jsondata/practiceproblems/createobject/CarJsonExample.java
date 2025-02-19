d86e3b31987f7051f49b0b5638e8b95e26dba1dd
Ankit_kumar <ankit95001kumar@gmail.com>
2025-02-19 11:00:20 +0530

    [Ankit] added Java object (Car) into JSON format

---
+++ src/main/java/com/ioprogramming/day02jsondata/practiceproblems/createobject/CarJsonExample.java
@@ -0,0 +1,21 @@
+package com.ioprogramming.day02jsondata.practiceproblems.createobject;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class CarJsonExample {
+    public static String convertCarToJson(Car car) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(car);
+        } catch (Exception e) {
+            return "Error converting to JSON: " + e.getMessage();
+        }
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Camry", 2022);
+        String carJson = convertCarToJson(myCar);
+        System.out.println("Car JSON:\n" + carJson);
+    }
+}
+
