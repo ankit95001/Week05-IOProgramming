1fcd72cdc908bcbc58ecbafc2f23b3766a8ce766
Ankit_kumar <ankit95001kumar@gmail.com>
2025-02-19 14:42:16 +0530

    [Ankit] added Test cases for Filter JSON data: Print only users older than 25 years.

---
+++ src/test/java/com/ioprogramming/day02jsondata/handsonpracticeproblems/filterrecord/JsonFilterExampleTest.java
@@ -0,0 +1,40 @@
+package com.ioprogramming.day02jsondata.handsonpracticeproblems.filterrecord;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JsonFilterExampleTest {
+
+    @Test
+    public void testFilterStudentsByAge() {
+        String jsonInput = "["
+                + "{\"name\":\"Ankit\",\"age\":24,\"email\":\"ankit@example.com\"},"
+                + "{\"name\":\"Aakrati\",\"age\":26,\"email\":\"aakrati@example.com\"},"
+                + "{\"name\":\"Anand\",\"age\":28,\"email\":\"anand@example.com\"},"
+                + "{\"name\":\"Yaman\",\"age\":30,\"email\":\"yaman@example.com\"}"
+                + "]";
+
+        List<Student> result =JsonFilterExample.filterStudentsByAge(jsonInput, 25);
+
+        assertEquals(3, result.size());
+        assertEquals("Aakrati", result.get(0).getName());
+        assertEquals("Anand", result.get(1).getName());
+        assertEquals("Yaman", result.get(2).getName());
+    }
+
+    @Test
+    public void testFilterNoMatch() {
+        String jsonInput = "["
+                + "{\"name\":\"Ankit\",\"age\":24,\"email\":\"ankit@example.com\"},"
+                + "{\"name\":\"Avinash\",\"age\":23,\"email\":\"avinash@example.com\"}"
+                + "]";
+
+        List<Student> result = JsonFilterExample.filterStudentsByAge(jsonInput, 25);
+        assertTrue(result.isEmpty());
+    }
+}
+
